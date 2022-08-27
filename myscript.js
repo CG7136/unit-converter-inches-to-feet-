ff22958bb30df1bb31608e22e7633bd82a8d4888
chaitanya <ccg6070@gmail.com>
2022-08-27 17:48:39 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,18 @@
+var feet = document.getElementById('feet');
+var inch = document.getElementById('inch');
+
+feet.addEventListener('input',function(){
+ console.log("channged");
+ let f =this.value;
+ let i = f*12;
+ inch.value = i;
+});
+inch.addEventListener('input',function(){
+    console.log("channged");
+    let i =this.value;
+    let f = i/12;
+    if(!Number.isInteger(f)){
+        f = f.toFixed(2);
+    };
+    feet.value = f;
+   });
